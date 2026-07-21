725a105f7f64f9f04259845afc005c081a94143d
Rashid <alshiblir650@gmail.com>
2026-07-20 21:44:45 -0700

    create java file3

---
+++ src/CircleInformation.java
@@ -0,0 +1,9 @@
+public class CircleInformation {
+    public  static   void main(String []args){
+
+        double radius = 7.5;
+        System.out.println("The radius of the circle is:"+ radius);
+
+
+    }
+}
