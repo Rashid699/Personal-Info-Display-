7f20291c3a24798ad91c191d4c43ded7fb1a4e20
Rashid <alshiblir650@gmail.com>
2026-07-20 22:14:32 -0700

    create java file5

---
+++ src/CarInformation.java
@@ -0,0 +1,21 @@
+public class CarInformation {
+    public  static   void main(String []args){
+
+        System.out.println("Car Information");
+        System.out.println("---------");
+
+        String brand = "Toyota";
+        String model = "Corolla";
+        Integer year = 2023;
+        double price = 22500.50;
+        boolean automatic = true;
+
+        System.out.println("Brand:"+ brand);
+        System.out.println("Model:"+ model);
+        System.out.println("Year:"+ year);
+        System.out.println("Price:"+ price);
+        System.out.println("Automatic:"+ automatic);
+
+
+    }
+}
